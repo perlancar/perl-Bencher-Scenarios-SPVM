2c1b42eaa308d8cc76a416a1bac43ec5e01497d1
perlancar (on pc-office) <perlancar@gmail.com>
2017-08-04 16:32:46 +0700

    Update to latest SPVM (no longer need to use SPVM::int or value() to get value)

--- lib/Bencher/Scenario/SPVM/Sum.pm
+++ lib/Bencher/Scenario/SPVM/Sum.pm
@@ -12,11 +12,11 @@ use SPVM 'Examples::Sum';
 our $scenario = {
     summary => 'Benchmark SPVM (sum two numbers)',
     modules => {
-        'SPVM' => {},
+        'SPVM' => {version => '0.0238'},
         'SPVM::Examples' => {}, # to pull SPVM/Examples/Sum.spvm
     },
     participants => [
-        { name => 'SPVM', code => sub { SPVM::Examples::Sum::sum(SPVM::int(3), SPVM::int(5))->value } },
+        { name => 'SPVM', code => sub { SPVM::Examples::Sum::sum(3, 5) } },
         { name => 'Perl', code => sub { My::Sum::sum(3, 5) } },
     ],
 };
